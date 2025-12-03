86e7dd8507a11b12b61ee7ca2fd5568d24390dd0
Elianny R. <csrodrigueze1@gmail.com>
2025-12-02 22:47:23 -0500

    db update

---
+++ matchUp_db.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE Match_Up;
+
+USE Match_Up;
+
+CREATE TABLE users(
+    userid INT AUTO_INCREMENT PRIMARY KEY,
+    user_email VARCHAR(100) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL,
+    user_pw VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE teams(
+    teamid INT AUTO_INCREMENT PRIMARY KEY,
+    team_name VARCHAR(50) NOT NULL,
+    home_city VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE matches(
+    macthid INT AUTO_INCREMENT PRIMARY KEY,
+    team_a VARCHAR(50) NOT NULL,
+    team_b VARCHAR(50) NOT NULL,
+    match_date DATETIME NOT NULL,
+    match_status VARCHAR(50) ENUM('Upcoming','Ongoing','Completed') DEFAULT 'Upcoming'
+);
