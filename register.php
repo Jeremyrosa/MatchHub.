139384d1eb9497627b59aa1a01bc261d41dd5dc2
Elianny R. <csrodrigueze1@gmail.com>
2025-12-07 21:08:57 -0500

    register.php update

--- register.php
+++ register.php
@@ -13,7 +13,19 @@ if ($conn->connect_error) {
     exit();
 }
 
-$stmt = $conn->prepare("SELECT id FROM users WHERE username = ?");
+$stmt = $conn->prepare("SELECT userid FROM users WHERE user_email = ?");
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$stmt->store_result();
+
+if ($stmt->num_rows > 0) {
+    echo json_encode(["success" => false, "message" => "Email already taken."]);
+    exit();
+}
+
+$stmt->close();
+
+$stmt = $conn->prepare("SELECT userid FROM users WHERE username = ?");
 $stmt->bind_param("s", $username);
 $stmt->execute();
 $stmt->store_result();
@@ -22,10 +34,9 @@ if ($stmt->num_rows > 0) {
     echo json_encode(["success" => false, "message" => "Username already taken"]);
     exit();
 }
-
 $stmt->close();
 
-$stmt = $conn->prepare("INSERT INTO users (email, username, userpw) VALUES (?, ?, ?)");
+$stmt = $conn->prepare("INSERT INTO users (user_email, username, userpw) VALUES (?, ?, ?)");
 $stmt->bind_param("sss", $email, $username, $password);
 
 if ($stmt->execute()) {
