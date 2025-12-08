e6e7553c48a55a514796c9c3a7860af62fb37fac
Elianny R. <csrodrigueze1@gmail.com>
2025-12-07 21:02:29 -0500

    register.php update

--- register.php
+++ register.php
@@ -8,6 +8,15 @@ $password = trim($data["password"]);
 
 $conn = new mysqli("localhost", "root", "", "Match_Up");
 
+if ($conn->connect_error) {
+    echo json_encode(["success" => false, "message" => "Connection error."]);
+    exit();
+}
+
+$stmt = $conn->prepare("SELECT id FROM users WHERE username = ?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$stmt->store_result();
 
 
 $stmt->close();
