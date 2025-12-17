369eb90262cc9f99d0a518df53d783cf9b850470
Elianny R. <csrodrigueze1@gmail.com>
2025-12-17 17:46:14 -0500

    db update

--- matchUp_db.sql
+++ matchUp_db.sql
@@ -13,7 +13,7 @@ CREATE TABLE admin(
     adminid INT AUTO_INCREMENT PRIMARY KEY,
     admin_email VARCHAR(100) NOT NULL UNIQUE,
     admin_username VARCHAR(50) NOT NULL UNIQUE,
-    adminpw VARCHAR(50) NOT NULL
+    adminpw VARCHAR(300) NOT NULL
 );
 
 CREATE USER 'matchup_user'@'localhost' INDENTIFIED BY 'secure_password';
